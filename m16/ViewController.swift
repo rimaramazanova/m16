20a4e6d68b9cb6a7fe20f12acb821ffb41ea0ae9
Rima <rimarz@yandex.ru>
2022-04-26 22:07:53 +0300

    second

---
+++ m16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Sadyk on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
